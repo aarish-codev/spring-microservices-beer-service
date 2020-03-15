10294f7a83ef158b7880718ba295b15f64f0daf7
AarishS <aarish.work@gmail.com>
2020-03-15 15:00:26 +0530

    initial setup

---
+++ src/main/java/aarish/spring/ms/beerservice/SpringMsBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package aarish.spring.ms.beerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aarish/spring/ms/beerservice/SpringMsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package aarish.spring.ms.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
